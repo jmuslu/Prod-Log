ae07c195f250a8c2cfea3f5050c754f4b9e9af06
jmuslu <your_email@example.com>
2025-01-27 15:18:09 -0500

    Initial Commit

---
+++ Prod Log/Prod_LogApp.swift
@@ -0,0 +1,32 @@
+//
+//  Prod_LogApp.swift
+//  Prod Log
+//
+//  Created by Joseph Muslu on 1/27/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Prod_LogApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Prod LogTests/Prod_LogTests.swift
@@ -0,0 +1,17 @@
+//
+//  Prod_LogTests.swift
+//  Prod LogTests
+//
+//  Created by Joseph Muslu on 1/27/25.
+//
+
+import Testing
+@testable import Prod_Log
+
+struct Prod_LogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
